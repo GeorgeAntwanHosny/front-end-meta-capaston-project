ef4d4690b96c693fb31f9e7ca0ac7eaecfae25c9
GeorgeAntwanHosny <georgeantwanhosnyghaly@outlook.com>
2023-05-02 11:03:24 +0200

    add responsive design

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -2,10 +2,11 @@ import Nav from "./Nav";
 import Logo from "./Logo";
 const Header = () => {
   return (
-    <>
+    <header>
+
       <Logo />
       <Nav />
-    </>
+    </header>
   );
 };
 export default Header;
--- src/components/Shared/Logo.js
+++ src/components/Shared/Logo.js
@@ -2,9 +2,9 @@ import { ReactComponent as MyIcon } from "../../assets/images/Logo.svg";
 
 const Logo = () => {
   return (
-    <header>
+    <>
       <MyIcon />
-    </header>
+    </>
   );
 };
 export default Logo;
